8f2778a275a44779c01847196c8f9d922719dd60
batuhan <107402252+batuhankanra@users.noreply.github.com>
2024-08-01 14:36:37 +0300

    blog view added

--- src/pages/home/blog/index.jsx
+++ src/pages/home/blog/index.jsx
@@ -1,66 +1,52 @@
 
+import { useEffect, useState } from 'react'
 import ppimg from '../../../assets/pp/wolf.jpg'
+import axios from 'axios'
 
 export default function Blog() {
+    const [blog,setBlog]=useState('')
+    console.log(blog)
+ ;
+    
+    useEffect(() => {
+        const fetchPosts = async () => {
+          try {
+            const response = await axios.get('http://localhost:3000/api/product/viewBlog');
+            const convertToTRDate =(isoDate)=>{
+                return new Date(isoDate).toLocaleString('tr-TR', { timeZone: 'Europe/Istanbul' });
+            }
+            const postsWithTRDates = response.data.posts.map(post => ({
+                ...post,
+                date: convertToTRDate(post.date)
+              }));
+              setBlog(postsWithTRDates)
+
+          } catch (err) {
+            console.log(err.message || 'An error occurred');
+          } 
+        };
+    
+        fetchPosts();
+      }, []);
   return (
     <div className='container mx-auto pt-2'>
         <div className='bg-fourth p-1 rounded-md'>
             <div className=' h-[500px] overflow-y-scroll bg-gray-100 border-b'>
-            <div className='flex flex-col  py-2  border-b  m-3'>
+            
+                {blog && blog.map(data=>(
+                    <div key={data._id} className='flex flex-col  py-2  border-b  m-3'>
                     <div className='flex items-center justify-between '>
-                        <h1 className='font-semibold text-lg'>Baslik</h1>
-                        <span className='text-sm text-gray-500'>12/03/2024</span>
+                        <h1 className='font-semibold text-lg'>{data.title}</h1>
+                        <span className='text-sm text-gray-500'>{data.date}</span>
                     </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat perferendis tempore ut ullam laudantium corrupti, quis provident ratione reiciendis esse dolorum possimus nisi minus sunt temporibus et unde modi repellat sint aliquam, reprehenderit maiores. Omnis fuga unde dolor esse quaerat iure quo voluptates. Incidunt inventore ipsum quod quia deserunt fugit doloribus consequuntur, consequatur vero laborum est quam accusantium ex eum porro libero magnam repellendus placeat non necessitatibus aut. Obcaecati laudantium architecto maxime, debitis labore iure, atque molestiae reprehenderit officia distinctio ab doloribus laboriosam iste facere incidunt officiis tempore cupiditate in, praesentium quibusdam optio modi perferendis quis quidem. Ab, accusamus quas.</p>
+                    <p>{data.desc} </p>
                     <div className='flex items-center gap-3 py-2'>
                         <img src={ppimg} className='w-20 object-cover h-20 rounded-full'  />
-                        <h1 className='text-base font-medium underline'>Kullanici adi</h1>
+                        <h1 className='text-base font-medium underline'>{data.username}</h1>
                     </div>
                 </div>
-                <div className='flex flex-col  py-2  border-b  m-3'>
-                    <div className='flex items-center justify-between '>
-                        <h1 className='font-semibold text-lg'>Baslik</h1>
-                        <span className='text-sm text-gray-500'>12/03/2024</span>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat perferendis tempore ut ullam laudantium corrupti, quis provident ratione reiciendis esse dolorum possimus nisi minus sunt temporibus et unde modi repellat sint aliquam, reprehenderit maiores. Omnis fuga unde dolor esse quaerat iure quo voluptates. Incidunt inventore ipsum quod quia deserunt fugit doloribus consequuntur, consequatur vero laborum est quam accusantium ex eum porro libero magnam repellendus placeat non necessitatibus aut. Obcaecati laudantium architecto maxime, debitis labore iure, atque molestiae reprehenderit officia distinctio ab doloribus laboriosam iste facere incidunt officiis tempore cupiditate in, praesentium quibusdam optio modi perferendis quis quidem. Ab, accusamus quas.</p>
-                    <div className='flex items-center gap-3 py-2'>
-                        <img src={ppimg} className='w-20 object-cover h-20 rounded-full'  />
-                        <h1 className='text-base font-medium underline'>Kullanici adi</h1>
-                    </div>
-                </div>
-                <div className='flex flex-col  py-2  border-b  m-3'>
-                    <div className='flex items-center justify-between '>
-                        <h1 className='font-semibold text-lg'>Baslik</h1>
-                        <span className='text-sm text-gray-500'>12/03/2024</span>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat perferendis tempore ut ullam laudantium corrupti, quis provident ratione reiciendis esse dolorum possimus nisi minus sunt temporibus et unde modi repellat sint aliquam, reprehenderit maiores. Omnis fuga unde dolor esse quaerat iure quo voluptates. Incidunt inventore ipsum quod quia deserunt fugit doloribus consequuntur, consequatur vero laborum est quam accusantium ex eum porro libero magnam repellendus placeat non necessitatibus aut. Obcaecati laudantium architecto maxime, debitis labore iure, atque molestiae reprehenderit officia distinctio ab doloribus laboriosam iste facere incidunt officiis tempore cupiditate in, praesentium quibusdam optio modi perferendis quis quidem. Ab, accusamus quas.</p>
-                    <div className='flex items-center gap-3 py-2'>
-                        <img src={ppimg} className='w-20 object-cover h-20 rounded-full'  />
-                        <h1 className='text-base font-medium underline'>Kullanici adi</h1>
-                    </div>
-                </div>
-                <div className='flex flex-col  py-2  border-b  m-3'>
-                    <div className='flex items-center justify-between '>
-                        <h1 className='font-semibold text-lg'>Baslik</h1>
-                        <span className='text-sm text-gray-500'>12/03/2024</span>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat perferendis tempore ut ullam laudantium corrupti, quis provident ratione reiciendis esse dolorum possimus nisi minus sunt temporibus et unde modi repellat sint aliquam, reprehenderit maiores. Omnis fuga unde dolor esse quaerat iure quo voluptates. Incidunt inventore ipsum quod quia deserunt fugit doloribus consequuntur, consequatur vero laborum est quam accusantium ex eum porro libero magnam repellendus placeat non necessitatibus aut. Obcaecati laudantium architecto maxime, debitis labore iure, atque molestiae reprehenderit officia distinctio ab doloribus laboriosam iste facere incidunt officiis tempore cupiditate in, praesentium quibusdam optio modi perferendis quis quidem. Ab, accusamus quas.</p>
-                    <div className='flex items-center gap-3 py-2'>
-                        <img src={ppimg} className='w-20 object-cover h-20 rounded-full'  />
-                        <h1 className='text-base font-medium underline'>Kullanici adi</h1>
-                    </div>
-                </div>
-                <div className='flex flex-col  py-2  border-b  m-3'>
-                    <div className='flex items-center justify-between '>
-                        <h1 className='font-semibold text-lg'>Baslik</h1>
-                        <span className='text-sm text-gray-500'>12/03/2024</span>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat perferendis tempore ut ullam laudantium corrupti, quis provident ratione reiciendis esse dolorum possimus nisi minus sunt temporibus et unde modi repellat sint aliquam, reprehenderit maiores. Omnis fuga unde dolor esse quaerat iure quo voluptates. Incidunt inventore ipsum quod quia deserunt fugit doloribus consequuntur, consequatur vero laborum est quam accusantium ex eum porro libero magnam repellendus placeat non necessitatibus aut. Obcaecati laudantium architecto maxime, debitis labore iure, atque molestiae reprehenderit officia distinctio ab doloribus laboriosam iste facere incidunt officiis tempore cupiditate in, praesentium quibusdam optio modi perferendis quis quidem. Ab, accusamus quas.</p>
-                    <div className='flex items-center gap-3 py-2'>
-                        <img src={ppimg} className='w-20 object-cover h-20 rounded-full'  />
-                        <h1 className='text-base font-medium underline'>Kullanici adi</h1>
-                    </div>
-                </div>
+                ))}
+                
             </div>
             
 
